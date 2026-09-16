c1b97b3913a7fe2d47e735679818eb68e7b8e7da
Gorincu Ioan-Alexandru <ioan.gorincu@stud.etti.upb.ro>
2026-09-16 13:10:45 +0300

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if s[l] != s[r]:
+                s1 = s[l + 1:r + 1]
+                s2 = s[l:r]
+                return s1 == s1[::-1] or s2 == s2[::-1]
+            l += 1
+            r -= 1
+        return True
